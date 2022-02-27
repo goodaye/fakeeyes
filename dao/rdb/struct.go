afbb511e9c5a08f7cca383d32ce58bb5345cb1ab
Your Name <matthewmu@tencent.com>
2022-02-27 15:00:04 +0800

    add more field for device collect

--- dao/rdb/struct.go
+++ dao/rdb/struct.go
@@ -34,23 +34,28 @@ type DeviceSession struct {
 
 // Device
 type Device struct {
-	ID             int64     `json:"id" xorm:"not null pk autoincr INT"`
-	UUID           string    `json:"uuid" xorm:" not null unique   VARCHAR(255) comment('设备唯一编码UUID')"`
-	SN             string    `json:"sn" xorm:"not null unique VARCHAR(255) comment('设备SN')"`
-	Name           string    `json:"name" xorm:"not null  VARCHAR(255) comment('设备名')"`
-	ModelName      string    `json:"model_name" xorm:" null  VARCHAR(255) comment('设备名,比如树莓派/MacBook')"`
-	ModelID        string    `json:"model_id" xorm:" null  VARCHAR(255) comment('设备名,比如树莓派4B/MacBook16,1')"`
-	ProcessorName  string    `json:"processor_name" xorm:" null  VARCHAR(255) comment('处理器名字')"`
-	ProcessorSpeed string    `json:"processor_speed" xorm:" null  VARCHAR(255) comment('处理器速度')"`
-	Manufacturer   string    `json:"manufacturer" xorm:" null  VARCHAR(255) comment('设备制造商,比如,亚博')"`
-	OSName         string    `json:"os_name" xorm:" null  VARCHAR(255) comment('OS名字')"`
-	OSVersion      string    `json:"os_version" xorm:" null  VARCHAR(255) comment('OS Version')"`
-	HardwareUUID   string    `json:"hardware_uuid" xorm:" null  VARCHAR(255) comment('硬件UUID')"`
-	Status         int       `json:"status" xorm:" null  tinyint comment('设备制状态，比如，在线、离线')"`
-	Uptime         int64     `json:"up_time " xorm:" null int comment('设备启动时间(单位秒)')"`
-	LastLogin      time.Time `json:"last_login" xorm:"not null default CURRENT_TIMESTAMP TIMESTAMP comment('上次登陆时间')" `
-	GmtCreated     time.Time `json:"gmt_created" xorm:"not null default '1970-01-01 08:00:01' TIMESTAMP created" `
-	GmtModified    time.Time `json:"gmt_modified" xorm:"not null default CURRENT_TIMESTAMP TIMESTAMP updated" `
+	ID           int64     `json:"id" xorm:"not null pk autoincr INT"`
+	UUID         string    `json:"uuid" xorm:" not null unique   VARCHAR(255) comment('设备唯一编码UUID')"`
+	SN           string    `json:"sn" xorm:"not null unique VARCHAR(255) comment('设备SN')"`
+	Name         string    `json:"name" xorm:"not null  VARCHAR(255) comment('设备名')"`
+	ModelName    string    `json:"model_name" xorm:" null  VARCHAR(255) comment('设备名,比如树莓派/MacBook')"`
+	ModelID      string    `json:"model_id" xorm:" null  VARCHAR(255) comment('设备名,比如树莓派4B/MacBook16,1')"`
+	CPUModel     string    `json:"cpu_model" xorm:" null  VARCHAR(255) comment('处理器型号')"`
+	CPUModelID   string    `json:"cpu_model_id" xorm:" null  VARCHAR(255) comment('处理器型号ID')"`
+	CPUSpeed     string    `json:"cpu_speed" xorm:" null  VARCHAR(255) comment('处理器速度')"`
+	CPUArch      string    `json:"cpu_arch" xorm:" null  VARCHAR(255) comment('处理器架构,x86, arm')"`
+	CPUVendor    string    `json:"cpu_vendor" xorm:" null  VARCHAR(255) comment('处理器速度')"`
+	CPUCores     int       `json:"cpu_cores" xorm:" null  INT comment('处理器核心数')"`
+	CPUSocket    int       `json:"cpu_socket" xorm:" null  INT comment('处理器Socket个数')"`
+	Manufacturer string    `json:"manufacturer" xorm:" null  VARCHAR(255) comment('设备制造商,比如,亚博')"`
+	OSName       string    `json:"os_name" xorm:" null  VARCHAR(255) comment('OS名字')"`
+	OSVersion    string    `json:"os_version" xorm:" null  VARCHAR(255) comment('OS Version')"`
+	HardwareUUID string    `json:"hardware_uuid" xorm:" null  VARCHAR(255) comment('硬件UUID')"`
+	Status       int       `json:"status" xorm:" null  tinyint comment('设备制状态,比如 在线/离线')"`
+	Uptime       int64     `json:"up_time " xorm:" null int comment('设备启动时间(单位秒)')"`
+	LastLogin    time.Time `json:"last_login" xorm:"not null default CURRENT_TIMESTAMP TIMESTAMP comment('上次登陆时间')" `
+	GmtCreated   time.Time `json:"gmt_created" xorm:"not null default '1970-01-01 08:00:01' TIMESTAMP created" `
+	GmtModified  time.Time `json:"gmt_modified" xorm:"not null default CURRENT_TIMESTAMP TIMESTAMP updated" `
 }
 
 // Room 房间
--- protos/request/device.go
+++ protos/request/device.go
@@ -2,16 +2,20 @@ package request
 
 // DeviceInfo 设备信息
 type DeviceInfo struct {
-	SN             string `json:"sn"  validate:"required"`
-	Name           string `json:"name" validate:"required"`
-	ModelName      string `json:"model_name" `
-	ModelID        string `json:"model_id" `
-	ProcessorName  string `json:"processor_name" `
-	ProcessorSpeed string `json:"processor_speed" `
-	Manufacturer   string `json:"manufacturer" `
-	OSName         string `json:"os_name" `
-	OSVersion      string `json:"os_version" `
-	HardwareUUID   string `json:"hardware_uuid"`
+	SN           string `json:"sn"  validate:"required"`
+	Name         string `json:"name" validate:"required"`
+	ModelName    string `json:"model_name" `
+	ModelID      string `json:"model_id" `
+	CPUModelID   string `json:"cpu_model_id"`
+	CPUSpeed     string `json:"cpu_speed" `
+	CPUArch      string `json:"cpu_arch" `
+	CPUVendor    string `json:"cpu_vendor" `
+	CPUCores     int    `json:"cpu_cores" `
+	CPUSocket    int    `json:"cpu_socket" `
+	Manufacturer string `json:"manufacturer" `
+	OSName       string `json:"os_name" `
+	OSVersion    string `json:"os_version" `
+	HardwareUUID string `json:"hardware_uuid"`
 }
 
 type DescribeDevice struct {
